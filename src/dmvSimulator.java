a31a29253c9f1e2fbc5312dffd53dd6bcafc3f29
Jacob Conrad <Jacob.Conrad@quinnipiac.edu>
2021-03-01 16:24:35 -0500

    Added dmv set up

--- src/dmvSimulator.java
+++ src/dmvSimulator.java
@@ -2,6 +2,21 @@
 public class dmvSimulator {
 	public static void main(String[] args)
 	{
+		System.out.println("Welcome to the DMV!");
+		int number = (int) (Math.random() * 100);
+		System.out.println("Your number is " + number + ", wait until you are called.");
 		
+		for(int i = number + 1; i <= 100; i++)
+		{
+			System.out.println(i);
+		}
+		
+		for(int i = 1; i < number; i++)
+		{
+			System.out.println(i);
+		}
+		
+		System.out.println("You don't have the required");
+		System.out.println("Exitting the program...");
 	}
 }
